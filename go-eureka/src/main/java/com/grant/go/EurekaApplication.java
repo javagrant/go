f056326a7cc4a4f0a058bc010ef0f411156c20ae
grant <ligaolong@aliyun.com>
2021-02-28 19:52:10 +0800

    【version】:1.0.0 【message】:commit eureka server 【note】:

---
+++ go-eureka/src/main/java/com/grant/go/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.grant.go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka server 启动入口
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
